5f422cd7fb734f1c94a631e260a5aef77bec3088
matlab-itachi <149710341+matlab-itachi@users.noreply.github.com>
2023-11-02 23:12:10 +0530

    Create max_min_sv.m

---
+++ max_min_sv.m
@@ -0,0 +1,26 @@
+clc
+syms x real
+f = input('Enter the function f(x): ');
+fx = diff(f,x);
+c = solve(fx);
+cmin = min(double(c));
+cmax = max(double(c));
+figure (1);
+ezplot(f, [cmin-2, cmax+2]);
+hold on;
+fxx = diff(fx, x);
+for i = 1:length(c)
+    T1 = subs(fxx, x, c(1));
+    T3 = subs(f, x, c(1));
+    if(double(T1)==0)
+        sprintf('The test fails at x=%d ', double(c(i)))
+    else if(double(T1)<0)
+        sprintf('The max point x is %d', double(c(i)))
+        sprintf('The value of the func is %d', double(T3))
+    else
+        sprintf('The min point x is %d', double(c(i)))
+        sprintf('The value of the func is %d', double(T3))
+    end
+    end
+    plot(double(c(i)), double(T3),'r','MarkerSize',15);
+end
