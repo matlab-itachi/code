a5f3c17a6990c4cc67dd22d6f59d299dbcece2b3
matlab-itachi <149710341+matlab-itachi@users.noreply.github.com>
2023-11-03 01:49:36 +0530

    max_min_mv.m

--- max_min_mv.m
+++ max_min_mv.m
@@ -14,29 +14,29 @@ fsurf(f);
 legstr = {'Function plot'};
 
 for i = 1:size(ax)
-  T1 = D(ax(i), ay(i));
-  T2 = r(ax(i), ay(i));
-  T3 = f(ax(i), ay(i));
-  if(double(T1)==0)
-    sprintf('At point (%f, %f) further investigation is required',ax(i), ay(i));
-    legstr = [legstr, {'Case of Further investigation'}];
-    mkr = 'ko';
-  else if(double(T1)<0)
-    sprintf('The point (%f, %f) is a saddle point', ax(i), ay(i));
-    legstr = [legstr, {'Saddle point'}];
-    mkr = 'bv';
-  else
-    if(double(T2)<0)
-      sprintf('The maximum value is at (%f, %f) = %f', ax(i), ay(i), T3);
-      legstr = [legstr, {'Maximum value'}];
-      mkr = 'g+';
-    else
-      sprintf('The minimum value is at (%f, %f) = %f', ax(i), ay(i), T3);
-      legstr = [legstr, {'Minimum value'}];
-      mkr = 'r*';
-    end
-  end
-  hold on
-  plot3(ax(i), ay(i), T3, mkr, 'Linewidth', 4)
-  end
-  legend(legstr, 'Location', 'Best');
+T1 = D(ax(i), ay(i));
+T2 = r(ax(i), ay(i));
+T3 = f(ax(i), ay(i));
+if(double(T1)==0)
+sprintf('At point (%f, %f) further investigation is required',ax(i), ay(i));
+legstr = [legstr, {'Case of Further investigation'}];
+mkr = 'ko';
+else if(double(T1)<0)
+sprintf('The point (%f, %f) is a saddle point', ax(i), ay(i));
+legstr = [legstr, {'Saddle point'}];
+mkr = 'bv';
+else
+if(double(T2)<0)
+sprintf('The maximum value is at (%f, %f) = %f', ax(i), ay(i), T3);
+legstr = [legstr, {'Maximum value'}];
+mkr = 'g+';
+else
+sprintf('The minimum value is at (%f, %f) = %f', ax(i), ay(i), T3);
+legstr = [legstr, {'Minimum value'}];
+mkr = 'r*';
+end
+end
+hold on
+plot3(ax(i), ay(i), T3, mkr, 'Linewidth', 4)
+end
+legend(legstr, 'Location', 'Best');
