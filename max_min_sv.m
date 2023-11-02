b8df7a06a5d9bcd0c61ca0f0f2d785fa521d4abf
itachi <149710341+matlab-itachi@users.noreply.github.com>
2023-11-03 04:12:41 +0530

    max_min_sv.m

--- max_min_sv.m
+++ max_min_sv.m
@@ -10,8 +10,8 @@ ezplot(f, [cmin-2, cmax+2]);
 hold on;
 fxx = diff(fx, x);
 for i = 1:length(c)
-    T1 = subs(fxx, x, c(1));
-    T3 = subs(f, x, c(1));
+    T1 = subs(fxx, x, c(i));
+    T3 = subs(f, x, c(i));
     if(double(T1)==0)
         sprintf('The test fails at x=%d ', double(c(i)))
     else if(double(T1)<0)
