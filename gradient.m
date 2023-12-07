6d0ca9a38f7e8441b2bb9c6a9481b2b663fc65e9
itachi <149710341+matlab-itachi@users.noreply.github.com>
2023-12-08 00:24:06 +0530

    Update gradient.m

--- gradient.m
+++ gradient.m
@@ -1,18 +1,15 @@
 clc 
 clear all
-syms x y
-f = input('Enter the the vector in the form i and j: ');
-F = gradient(f);
-P = inline(vectorize(F(1)), 'x', 'y');
-Q = inline(vectorize(F(2)), 'x', 'y');
-x = linspace(-2, 2, 10);
-y = x;
-[X, Y] = meshgrid(x, y);
-U = P(X, Y);
-V = Q(X, Y);
-quiver(X, Y, U, V, 1)
+syms x y 
+f = input('Enter the function: ');
+F = gradient(f, [x,y])
+P(x,y) = F(1); Q(x,y) = F(2);
+x = linspace(-2, 2, 10); y=x;
+[X, Y]=meshgrid(x, y);
+U = P(X, Y); V=Q(X,Y);
+quiver(X, Y, U, V, 1);
 axis on
 xlabel('x')
 ylabel('y')
 hold on
-ezcontour(f, [-2,2])
+fcontour(f, [-2, 2])
