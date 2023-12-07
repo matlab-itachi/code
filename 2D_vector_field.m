dcd9ce77412927b893405aa5869386790930cd6a
itachi <149710341+matlab-itachi@users.noreply.github.com>
2023-12-08 01:14:48 +0530

    Update 2D_vector_field.m

--- 2D_vector_field.m
+++ 2D_vector_field.m
@@ -1,16 +1,9 @@
-clc
+clc 
 clear all
-syms x y z
-
-F = input("Enter the vector field in the form i, j, k: ");
-P = inline(vectorize(F(1)), 'x', 'x');
-Q = inline(vectorize(F(2), 'x', 'y');
-x = linspace(-1, 1, 10);
-y=x;
-[X, Y] = meshgrid(x, y);
-U = P(X, Y);
-V = Q(X, Y);
-quiver(X, Y, U, V, 1)
-axis on 
-xlabel('x')
-ylabel('y')
+syms x y
+f = input('enter the vector field: ');
+P(x,y)=f(1); Q(x,y)=f(2);
+x=linspace(-4, 4, 20); y=x;
+[X,Y]=meshgrid(x,y);
+U=P(X,Y); V=Q(X,Y);
+quiver(X,Y,U,V)
